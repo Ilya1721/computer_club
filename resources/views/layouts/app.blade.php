59182fbc131ae2cd1a568a202b688b5641b2fe9c
unknown <ilyavusnya@gmail.com>
2019-12-17 15:02:07 +0200

    Fixed colors for navbar links

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -40,7 +40,10 @@
     <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
       <div class="container">
         <a class="navbar-brand" href="{{ url('/') }}">
+          <img id="logo" src="/img/logo.png" />
+          <span id="logo-name">
             {{ config('app.name', 'Laravel') }}
+          </span>
         </a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
             <span class="navbar-toggler-icon"></span>
@@ -57,11 +60,11 @@
             <!-- Authentication Links -->
             @guest
               <li class="nav-item">
-                  <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                  <a class="nav-link" id="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
               </li>
               @if (Route::has('register'))
                 <li class="nav-item">
-                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                    <a class="nav-link" id="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
                 </li>
               @endif
             @else
