5be663eb83138101ec799244ce9d9623646c0113
unknown <ilyavusnya@gmail.com>
2019-12-17 22:21:33 +0200

    Cleaned css

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,25 +2,25 @@
 @section('content')
 <div class="container text-center">
   <div class="row mt-5">
-    <div id="left-info" class="col-4 text-justify">
+    <div class="col-4 text-justify text-yellow">
       <span id="welcome-title">{{ config('app.name') }}</span>
-      <span id="site-desc">- найкращий комп`ютерний клуб міста<br/></span>
+      <span class="text-white">- найкращий комп`ютерний клуб міста<br/></span>
       <hr id="site-hr" />
       <span id="address-title">Адреса:</span> Разіна 17<br/>
       ТЦ Метроград, 2-й поверх<br/>
       <span id="address-title">телефон:</span> +380(93)247-56-47<br/>
       <span id="address-title">Режим роботи:</span> Кожний день. 9.00-24.00
       <span id="category">Анонси</span>
-      <a id="activity" href="#">
+      <a class="text-white" href="#">
         18.12.2019 з 10.00.
         Турнір по <span id="game-name">CS GO</span>
       </a><br />
-      <a id="activity" href="#">
+      <a class="text-white" href="#">
         25.12.2019 з 10.00.
         Турнір по <span id="game-name">Dota 2</span>
       </a><br />
       <span id="category">Ігри</span>
-      <div class="categories">
+      <div class="text-white">
         CS GO<br/>
         Dota 2<br/>
         League of Legends<br/>
@@ -31,10 +31,10 @@
         Serious Sam 2<br/>
         FIFA 2018<br/>
         PES 2018<br/>
-        <a id="other-games" href="#">Та інші...</a><br />
+        <a class="text-white" href="#">Та інші...</a><br />
       </div>
       <span id="category">Платформи</span>
-      <div class="categories">
+      <div class="text-white">
         ПК<br/>
         XBox360<br/>
         PS4<br/>
