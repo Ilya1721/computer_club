ca42ede4746e0f194f9e125543579cfdd47ff3df
unknown <ilyavusnya@gmail.com>
2019-12-17 19:43:10 +0200

    Added activities

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,6 +10,15 @@
       ТЦ Метроград, 2-й поверх<br/>
       <span id="address-title">телефон:</span> +380(93)247-56-47<br/>
       <span id="address-title">Режим роботи:</span> Кожний день. 9.00-24.00
+      <span id="category">Анонси</span>
+      <a id="activity" href="#">
+        18.12.2019 з 10.00.
+        Турнір по <span id="game-name">CS GO</span>
+      </a><br />
+      <a id="activity" href="#">
+        25.12.2019 з 10.00.
+        Турнір по <span id="game-name">Dota 2</span>
+      </a><br />
     </div>
     <div class="col-8 text-left">
       <img id="main-img" src="/img/main.png" />
