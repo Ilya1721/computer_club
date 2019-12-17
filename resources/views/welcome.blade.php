b57fde575f62cf381c421425b5210940370e3605
unknown <ilyavusnya@gmail.com>
2019-12-17 16:22:55 +0200

    Added background image to body

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,6 +1,16 @@
 @extends('layouts.app')
 @section('content')
-<div class="container">
-  <h2>Club</h2>
+<div class="container text-center">
+  <h1 id="welcome-title">Комп`ютерний клуб {{ config('app.name') }}</h1>
+  <div class="row">
+    <div class="col-2">
+
+    </div>
+    <div class="col-8 text-center">
+
+    </div>
+    <div class="col-2">
+
+    </div>
 </div>
 @endsection
