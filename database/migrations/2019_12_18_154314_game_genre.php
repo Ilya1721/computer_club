e8e3dfdc4edeb937cd7a19872f7bdd435614f07b
unknown <ilyavusnya@gmail.com>
2019-12-18 17:45:25 +0200

    Added genre model and game_genre pivot table

---
+++ database/migrations/2019_12_18_154314_game_genre.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class GameGenre extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+      Schema::create('game_genre', function (Blueprint $table) {
+          $table->bigIncrements('id');
+          $table->unsignedBigInteger('game_id');
+          $table->unsignedBigInteger('genre_id');
+          $table->timestamp('updated_at')->useCurrent();
+          $table->timestamp('created_at')->useCurrent();
+      });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('game_genre');
+    }
+}
