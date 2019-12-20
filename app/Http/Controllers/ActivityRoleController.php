5b8e0f55822865b80804b479ecce1b2c1c05e2dc
unknown <ilyavusnya@gmail.com>
2019-12-20 21:26:45 +0200

    Added activity roles index

--- app/Http/Controllers/ActivityRoleController.php
+++ app/Http/Controllers/ActivityRoleController.php
@@ -3,8 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\ActivityRole;
 
 class ActivityRoleController extends Controller
 {
-    //
+    public function index()
+    {
+      $activity_roles = ActivityRole::paginate(5);
+
+      return view('activity_roles', [
+        'activity_roles' => $activity_roles,
+      ]);
+    }
 }
